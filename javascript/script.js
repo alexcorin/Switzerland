dfb6e07559e6a22fa764e7de8dd0e72b46c43024
DS-NTI <david.sodersen@elev.ga.ntig.se>
2021-05-20 17:04:46 +0200

    Saker i JS

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,12 @@
 function selector() {
     document.getElementById("Home").style.backgroundColor = "red";
 }
+
+//För att ändra bakgrunds bild på Culture sidan
+
+function SelectCulture(){
+    var culturePics = true;
+    while(culturePics==true){
+        alert("Ok");
+    }
+}
