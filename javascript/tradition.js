3d7a862509be96c968c6444e25c3cec68567537c
Johan-Tran <johan.tran@elev.ga.ntig.se>
2021-05-21 00:12:05 +0200

    a

--- javascript/tradition.js
+++ javascript/tradition.js
@@ -1,5 +1,5 @@
 const image = document.getElementById("pic");
-let images = ["css/devilbridge.jpeg", "css/matterhorn.jpeg"], i = 0;
+let images = ["img/devilbridge.jpeg", "img/mtterhorn.jpeg"], i = 0;
 
 function RandomPic() {
     i < images.length ? i += 1 : i = 0;
