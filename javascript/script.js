f6c84d8ba560a8c6afae56dc72055b209f66b8ea
DS-NTI <david.sodersen@elev.ga.ntig.se>
2021-05-20 17:20:26 +0200

    Culture JAva

--- javascript/script.js
+++ javascript/script.js
@@ -2,12 +2,3 @@
 function selector() {
     document.getElementById("Home").style.backgroundColor = "red";
 }
-
-//För att ändra bakgrunds bild på Culture sidan
-
-function SelectCulture(){
-    var culturePics = true;
-    while(culturePics==true){
-        alert("Ok");
-    }
-}
