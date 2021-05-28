1f81d18a5e9298a884b58fb490bcc31f60441d7c
DS-NTI <david.sodersen@elev.ga.ntig.se>
2021-05-28 10:44:06 +0200

    Culture Saker

--- javascript/culture.js
+++ javascript/culture.js
@@ -6,8 +6,30 @@ function SelectCulture(){
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "cover";
 }
+function SelectFood(){
+    var backgrounds = ["img/food2.jpg","img/food1.jpeg", "img/food4.jpg"];
+    var imgShown = document.body.style.backgroundImage;
+    var newNumber = Math.floor(Math.random()*(backgrounds.length));
+    document.body.style.background = 'url('+backgrounds[newNumber]+')';
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+}
+
+function SelectWatch(){
+    var backgrounds = ["img/watch1.jpeg"];
+    var imgShown = document.body.style.backgroundImage;
+    var newNumber = Math.floor(Math.random()*(backgrounds.length));
+    document.body.style.background = 'url('+backgrounds[newNumber]+')';
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+}
+
+function SelectSport(){
+    var backgrounds = ["img/skiing1.jpg"];
+    var imgShown = document.body.style.backgroundImage;
+    var newNumber = Math.floor(Math.random()*(backgrounds.length));
+    document.body.style.background = 'url('+backgrounds[newNumber]+')';
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+}
 window.onload = changeBg;
-(function timer() {
-    SelectCulture();
-    setInterval(SelectCulture, 3000);
-}());
