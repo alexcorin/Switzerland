9fc73aa1d39643e623328a7bb15b4f3f1f0a5b41
Johan-Tran <johan.tran@elev.ga.ntig.se>
2021-05-31 21:52:09 +0200

    kommentarer

--- javascript/tradition.js
+++ javascript/tradition.js
@@ -1,10 +1,12 @@
 var btn = document.getElementById("button");
 
+//Funktionen för knappen som gör att man kommer till toppen av hemsidan.
 function buttonUp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
+//Funktion som kollar när man når når längst nere av hemsidan och ändrar opacity av uppknappen
 window.onscroll = function () {
     if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
         btn.style.setProperty("--opacity", "0.5");
