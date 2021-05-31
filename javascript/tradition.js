5946edfc56f89a6096a57e5df47ca2b8fb5c09e0
Johan-Tran <johan.tran@elev.ga.ntig.se>
2021-05-31 21:19:00 +0200

    tradition

--- javascript/tradition.js
+++ javascript/tradition.js
@@ -1,12 +1,12 @@
+var btn = document.getElementById("button");
+
 function buttonUp() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
-
 window.onscroll = function () {
-    let btn = document.getElementById("button");
-    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 100) {
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
         btn.style.setProperty("--opacity", "0.5");
     }
     else {
